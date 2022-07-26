05154f31c9220b8402b1057656c8c1aa37148c70
Bruno Oliveira <nicoddemus@gmail.com>
2022-07-26 15:03:14 -0300

    Add Python 3.10 support

--- setup.py
+++ setup.py
@@ -36,6 +36,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     description="OOP Extensions is a set of utilities for object oriented programming not found on Python's standard library.",
     extras_require=extras_require,
